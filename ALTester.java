8e551ebe16d74ad8c2f500575d7ce7155c39ae9e
Tasnim <tchowdhury30>
2021-12-10 14:00:42 -0500

    still workin on it

--- ALTester.java
+++ ALTester.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class ALTester{
-	
+
+	public static void main ( String[] args){
+
+		ArrayList<Integer> _data = new ArrayList<Integer> (23);
+	//	fghfghgfh
+	}
 }
