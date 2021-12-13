cd060dc6d322ee0fc32ad18409970a6cfc40f434
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-12-12 22:14:47 -0500

    Update OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,1 +1,59 @@
-adsjhdaskjhdakjshdkj
+import java.util.ArrayList;
+
+public class OrderedArrayList {
+  //Tasks to do:
+  //Make add
+  //Make remove
+  //Make constructor that makes sure array is sorted in the begininng???????
+  
+  //instance vars:
+  ArrayList<Integer> _data;
+  
+  public OrderedArrayList() {
+    _data = new ArrayList<Integer>(); 
+  }
+  
+  public OrderedArrayList( ArrayList<Integer> list){ 
+    _data = list;
+    //Automatically sort the array once called
+    for (int i=0; i < _data.size() - 1; i++) { // iteriate through _data 
+        for (int j = 0; j < _data.size() - 1; j++) { // iteriate through _data for each element of _data
+          if (_data.get(j) > _data.get(j+1)) { // if an element is greater than an element to the right of it
+            int temp = _data.get(j); // switch the elements' indexes 
+            _data.set(j, _data.get(j+1));
+            _data.set(j+1, temp); 
+          } 
+        }
+    }
+  }
+  
+ /**
+  public OrderedArrayList { Check if sorted
+      for (int i=0; i < _data.size() - 1; i++) { // iteriate through _data 
+        for (int j = 0; j < _data.size() - 1; j++) { // iteriate through _data for each element of _data
+          if (_data[j] > _data[j+1]) { // if an element is greater than an element to the right of it
+            int temp = _data[j]; // switch the elements' indexes 
+            _data[j] = _data[j+1];
+            _data[j+1] = temp; }
+        }
+      }
+  }
+  **/
+  
+  public void add(int newVal){ 
+    //determine where newVal belongs.
+    for (int i = 0; i < _data.size() -1; i++) {
+      if (newVal < _data.get(i)) { //SEEMS okay lets try it!!!
+        _data.add(i, newVal);
+        return;
+      }
+    }
+      //newVal is bigger than all elements of the array. 
+      //Add newVal at the leftmost cell.
+      _data.add(newVal);
+  }
+  
+  public static void main (String[] args){
+      System.out.println("Hello World");
+  }
+}
